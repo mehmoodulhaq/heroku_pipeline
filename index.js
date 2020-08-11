f8edb5139e4efe6a6cd1f9bca4b23195f45eee56
Romesa Mehmoodulhaq <dev.romesa@gmail.com>
2020-08-11 14:53:51 +0500

    (heroku) deploy test app

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+}) 
